ef3e3fc13d0b79848d03cd37dabf256b5bc415ce
OALO <120587475+Mohd-khair-Alshrouf@users.noreply.github.com>
2023-02-07 21:12:18 +0100

    Update alt berechnen.py

--- alt berechnen.py
+++ alt berechnen.py
@@ -49,9 +49,9 @@ def ca():
 
     Label(f1,text=f"{b}              ",font=2,bg="#A2D9CE").place(y=130,x=-7)
     Label(f1, text="year", font=2,bg="#A2D9CE").place(y=127, x=60)
-    Label(f1, text=f"{c}               ", font=2,bg="#A2D9CE").place(y=160, x=-7)
+    Label(f1, text=f"{c}               ", font=2,bg="#A2D9CE").place(y=160, x=0)
     Label(f1, text="month", font=2,bg="#A2D9CE").place(y=158, x=60)
-    Label(f1, text=f"{d}                ", font=2,bg="#A2D9CE").place(y=190, x=-7)
+    Label(f1, text=f"{d}                ", font=2,bg="#A2D9CE").place(y=190, x=0)
     Label(f1, text="day", font=2,bg="#A2D9CE").place(y=187, x=60)
 
 
@@ -61,4 +61,5 @@ def ca():
 b1 = Button(o,text="Calculate",bg="#58D68D",font=2,width=25,height=0,command=ca).place(x=7,y=300)
 
 o.mainloop()
+
 
