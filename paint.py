b6732a8516b18cab11cb65476c7f622ccaba40ec
OALO <120587475+Mohd-khair-Alshrouf@users.noreply.github.com>
2023-02-18 20:04:39 +0100

    Update paint.py

--- paint.py
+++ paint.py
@@ -82,8 +82,13 @@ def whitees(evevt):
   x2, y2 = (evevt.x + 2000), (evevt.y + 2000)
   c.create_oval (x1,y1,x2,y2,fill=color,outline=color)
 
+def blackss(evevt):
+  color="black"
+  x1,y1=(evevt.x-2000) , (evevt.y-2000)
+  x2, y2 = (evevt.x + 2000), (evevt.y + 2000)
+  c.create_oval (x1,y1,x2,y2,fill=color,outline=color)
+
 master = Tk()
-master.title("Beginner paint")
 def reda():
   c.bind("<B1-Motion>",red)
 
@@ -118,62 +123,56 @@ def pinka():
   c.bind("<B1-Motion>", pink)
 
 def clear():
-  c.bind("<B1-Motion>", whitees)
+  c.delete("all")
 
 
-f = Frame(master,width=60,height=700,bg="white")
-f.place(x=20,y=50)
 
-red1 = Button(f,width=4,height=2,bg="red",command=reda)
-red1.place(y=7,x=15)
 
-blue2 = Button(f,width=4,height=2,bg="blue",command=bluea)
-blue2.place(y=60,x=15)
 
-black3 = Button(f,width=4,height=2,bg="black",command=blacka)
-black3.place(y=115,x=15)
+red1 = Button(master,width=4,height=2,bg="red",command=reda)
+red1.place(y=360,x=15)
 
-green4 = Button(f,width=4,height=2,bg="green",command=greena)
-green4.place(y=170,x=15)
+blue2 = Button(master,width=4,height=2,bg="blue",command=bluea)
+blue2.place(y=310,x=60)
 
-yellow5 = Button(f,width=4,height=2,bg="yellow",command=yellowa)
-yellow5.place(y=225,x=15)
+black3 = Button(master,width=4,height=2,bg="black",command=blacka)
+black3.place(y=110,x=15)
 
-white6 = Button(f,width=4,height=2,bg="white",command=whitea)
-white6.place(y=280,x=15)
+green4 = Button(master,width=4,height=2,bg="green",command=greena)
+green4.place(y=110,x=60)
 
-orange7 = Button(f,width=4,height=2,bg="orange",command=orangea)
-orange7.place(y=340,x=15)
+yellow5 = Button(master,width=4,height=2,bg="yellow",command=yellowa)
+yellow5.place(y=160,x=15)
 
-purple8 = Button(f,width=4,height=2,bg="purple",command=purplea)
-purple8.place(y=400,x=15)
+white6 = Button(master,width=4,height=2,bg="white",command=whitea)
+white6.place(y=160,x=60)
 
-brown9 = Button(f,width=4,height=2,bg="brown",command=browna)
-brown9.place(y=455,x=15)
+orange7 = Button(master,width=4,height=2,bg="orange",command=orangea)
+orange7.place(y=210,x=15)
 
-gray10 = Button(f,width=4,height=2,bg="gray",command=graya)
-gray10.place(y=510,x=15)
+purple8 = Button(master,width=4,height=2,bg="purple",command=purplea)
+purple8.place(y=210,x=60)
 
-pink11 = Button(f,width=4,height=2,bg="pink",command=pinka)
-pink11.place(y=570,x=15)
+brown9 = Button(master,width=4,height=2,bg="brown",command=browna)
+brown9.place(y=260,x=15)
 
-b5 = Button(master,text="clear",width=10,height=2,command=clear)
+gray10 = Button(master,width=4,height=2,bg="gray",command=graya)
+gray10.place(y=260,x=60)
+
+pink11 = Button(master,width=4,height=2,bg="pink",command=pinka)
+pink11.place(y=310,x=15)
+
+eraser = Button(master,width=10,height=2,text="Eraser",command=whitea)
+eraser.place(y=60,x=20)
+
+b5 = Button(master,text="clear all",width=10,height=2,command=clear)
 b5.place(x=20,y=5)
 
-l1 = Label(master,text="for to use clear press <clear> then press of any area and move the mouse on master ",font=2)
-l1.place(x=100,y=10)
 
 
 c = Canvas(master,width=1500,height=800,bg="white")
-c.place(x=100,y=50)
+c.place(x=120,y=0)
 c.bind("<B1-Motion>",paint)
 
-
-
-
-
-
-
 master.mainloop()
-
 
